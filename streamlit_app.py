97b020782e812d14740867db7e8423cd325b0f66
Zhangcj330 <102630975+Zhangcj330@users.noreply.github.com>
2023-12-07 17:19:04 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,12 @@ import streamlit
 
 
 streamlit.title("My Parents New Health Dinner")
+
+
+
+streamlit.header("Breakfast Menu")
+streamlit.text("Omega 3 & Blueberry Oatmeal")
+streamlit.text("Kale, Spinach & rocket Smoothie")
+streamlit.text("Hard-Boiled Free-Range Egg")
+
+
